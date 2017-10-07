edd00c2e44bcb23faf3d631a7820b72aff963321
dug9 <highaspirations@hotmail.com>
2017-10-07 11:44:41 -0600

    just starting on DIS, haven't done anything real yet

--- freex3d/src/lib/scenegraph/Component_DIS.c
+++ freex3d/src/lib/scenegraph/Component_DIS.c
@@ -25,3 +25,64 @@
 
 *********************************************************************/
 
+
+#include <config.h>
+#include <system.h>
+#include <display.h>
+#include <internal.h>
+#include <iglobal.h>
+#include <libFreeWRL.h>
+
+#include "../vrml_parser/Structs.h"
+#include "../vrml_parser/CRoutes.h"
+#include "../main/headers.h"
+
+#include "../world_script/fieldSet.h"
+#include "../x3d_parser/Bindable.h"
+#include "Collision.h"
+#include "quaternion.h"
+#include "Viewer.h"
+#include "../opengl/Frustum.h"
+#include "../opengl/Material.h"
+#include "../opengl/OpenGL_Utils.h"
+#include "../input/EAIHelpers.h"	/* for newASCIIString() */
+
+#include "Polyrep.h"
+#include "LinearAlgebra.h"
+#include "Children.h"
+
+/*
+typedef struct pComponent_DIS{
+	int something;
+}* ppComponent_DIS;
+void *Component_DIS_constructor(){
+	void *v = MALLOCV(sizeof(struct pComponent_DIS));
+	memset(v,0,sizeof(struct pComponent_DIS));
+	return v;
+}
+void Component_DIS_init(struct tComponent_DIS *t){
+	//public
+	//private
+	t->prv = Component_DIS_constructor();
+	{
+		ppComponent_DIS p = (ppComponent_DIS)t->prv;
+		p->something = 0;
+	}
+}
+void Component_DIS_clear(struct tComponent_DIS *t){
+	//public
+}
+
+http://www.web3d.org/documents/specifications/19775-1/V3.3/Part01/components/dis.html
+https://github.com/open-dis/open-dis-cpp
+http://www.web3d.org/x3d/content/examples/Basic/DistributedInteractiveSimulation/
+https://en.wikipedia.org/wiki/Distributed_Interactive_Simulation
+http://open-dis.sourceforge.net/Open-DIS.html
+
+
+
+
+*/
+
+
+
