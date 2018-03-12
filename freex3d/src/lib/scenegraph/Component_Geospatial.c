13592cfcca7319ca65ea1cadcf9540865605ffe7
dug9 <highaspirations@hotmail.com>
2018-03-12 16:14:41 -0600

    compile_geolocation > don't need so much in compile_ because doing it in prep_

--- freex3d/src/lib/scenegraph/Component_Geospatial.c
+++ freex3d/src/lib/scenegraph/Component_Geospatial.c
@@ -2851,7 +2851,7 @@ void compile_GeoLocation (struct X3D_GeoLocation * node) {
 				node->__localOrient.c[3]);
 		//#endif
 	}
-	if(MAR12){
+	if(0) if(MAR12){
 		//cylce test - should be able to transform elsewhere and back
 		// with only numerical noise difference.
 		struct SFVec3d gcCoords, gdCoords, userCoords, lcsCoords;
