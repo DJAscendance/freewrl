6c89d81e1361f02959b57caada1ae5494fc57f5d
dug9 <doug@node-1w7jr9y5mns5h467na6d82yqh.ipv6.telus.net>
2018-05-29 15:28:06 -0600

    JS scripting working via duktape

--- OSX_gui/FreeWRL-Desktop/FreeWRL/config.h
+++ OSX_gui/FreeWRL-Desktop/FreeWRL/config.h
@@ -31,6 +31,7 @@
 #define HAVE_GLU_TESS 1
 #define STATUSBAR_STD 1
 #define HAVE_LIBCURL 1
+#define JAVASCRIPT_DUK 1
 
 #define FREEWRL_MESSAGE_WRAPPER "/usr/bin/say"
 #define	BROWSER		"/usr/bin/open"
