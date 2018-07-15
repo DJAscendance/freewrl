950d38a1e9b304a593e775e40cd2731709c93a92
dug9 <highaspirations@hotmail.com>
2018-07-15 13:34:11 -0600

    touchup to .desktop

--- linux_appimage/run_per_freewrl_build.sh
+++ linux_appimage/run_per_freewrl_build.sh
@@ -8,7 +8,7 @@ cp /usr/local/bin/freewrl freewrl.AppDir/usr/bin
 -d freewrl.AppDir/usr/lib \
 -p '@executable_path/../lib' \
 -of \
--od 
-# -i /usr/lib
+-od
+ #-i /usr/lib
 # package into .appimage
 ./appimagetool freewrl.AppDir
