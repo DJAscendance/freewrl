0edebc71815ac6a9f2305ca1d817962d7f2073fd
dug9 <highaspirations@hotmail.com>
2020-04-13 16:08:28 -0600

    linestyle . line end style > arrow end working
    - need arrow (and dot) start

--- freex3d/src/lib/opengl/Compositing_Shaders.c
+++ freex3d/src/lib/opengl/Compositing_Shaders.c
@@ -1068,10 +1068,18 @@ bool on_linetype(inout vec4 frag_color){ \n\
 				//uend.s = length(f_next - f_prev); \n\
 				//must be end 2.0 \n\
 				if(u_linestrip_end_style == 1){ \n\
-					frag_color.r = 1.0; \n\
+					//arrow end \n\
+					frag_color.gb = vec2(0.0); \n\
+					float arrowlength = 14.0; \n\
+					vec2 head = vec2(uend.s - arrowlength,0.0); \n\
+					if(ubar.s > head.s){ \n\
+						on = false; \n\
+						vec2 range = ubar - head; \n\
+						float d = 2.0*range.t + range.s; \n\
+						if(d < arrowlength) on = true; \n\
+					} \n\
 				} else if(u_linestrip_end_style == 2){ \n\
 					//round end \n\
-					frag_color.gb = vec2(0.0); \n\
 					float radius = 6.0; \n\
 					vec2 center = vec2(0.0); \n\
 					center.s = (uend.s- radius); \n\
