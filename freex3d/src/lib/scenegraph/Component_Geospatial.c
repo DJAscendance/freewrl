0484e9c82bbcdc513f11d9bc32e164595fdd3d88
dug9 <highaspirations@hotmail.com>
2020-05-05 07:40:45 -0600

    displayBBox > GeoLOD done but not tested

--- freex3d/src/lib/scenegraph/Component_Geospatial.c
+++ freex3d/src/lib/scenegraph/Component_Geospatial.c
@@ -2903,6 +2903,14 @@ void child_GeoLOD (struct X3D_GeoLOD *node) {
 		printf ("hmmm - GeoLOD %p was level %d, now %d\n",node,node->__level, p->geoLodLevel);
 	}
 
+
+	if(fwl_getDrawBoundingBoxes()>1){
+		push_group_extent_default();
+	}else if(renderstate()->render_geom && node->displayBBox) {
+		draw_bbox(node->bboxCenter.c,node->bboxSize.c);
+	}
+	push_group_visible( node->visible && peek_group_visible());
+
 	#ifdef VERBOSE
 	if ( node->__inRange) {
 		printf ("GeoLOD %u (level %d) closer\n",node,p->geoLodLevel);
@@ -2984,6 +2992,20 @@ void child_GeoLOD (struct X3D_GeoLOD *node) {
 		p->geoLodLevel--;
 
 	}
+
+	pop_group_visible();
+	if(fwl_getDrawBoundingBoxes()>1){
+		//bbox - in child-space - gets transformed/propagated to Transform parent space and set as Transform._extent
+		extent6f2bbox(peek_group_extent(),node->bboxCenter.c,node->bboxSize.c);
+		if(renderstate()->render_geom && (node->displayBBox || (fwl_getDrawBoundingBoxes() % 2 == 1))) {
+			draw_bbox(node->bboxCenter.c,node->bboxSize.c);
+		}
+		//propagate bbox up one level
+		extent6f_copy(node->_extent,peek_group_extent());
+		pop_group_extent(); // up where parents are
+		union_group_extent(node->_extent); //
+	}
+
 }
 
 /************************************************************************/
