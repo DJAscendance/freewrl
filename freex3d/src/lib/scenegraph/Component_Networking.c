0c215e8a4a0cd1b1a3e0eb031b2fdbaff89b9e1d
dug9 <highaspirations@hotmail.com>
2020-05-04 18:23:47 -0600

    displayBBox > Anchor working

--- freex3d/src/lib/scenegraph/Component_Networking.c
+++ freex3d/src/lib/scenegraph/Component_Networking.c
@@ -496,8 +496,29 @@ void child_Anchor (struct X3D_Anchor *node) {
 	//LOCAL_LIGHT_CHILDREN(node->children);
 	prep_sibAffectors((struct X3D_Node*)node,&node->__sibAffectors);
 
+	if(fwl_getDrawBoundingBoxes()>1){
+		push_group_extent_default();
+	}else if(renderstate()->render_geom && node->displayBBox) {
+		draw_bbox(node->bboxCenter.c,node->bboxSize.c);
+	}
+	push_group_visible( node->visible && peek_group_visible());
+
 	/* now, just render the non-directionalLight children */
 	normalChildren(node->children);
+
+	pop_group_visible();
+	if(fwl_getDrawBoundingBoxes()>1){
+		//bbox - in child-space - gets transformed/propagated to Transform parent space and set as Transform._extent
+		extent6f2bbox(peek_group_extent(),node->bboxCenter.c,node->bboxSize.c);
+		if(renderstate()->render_geom && (node->displayBBox || (fwl_getDrawBoundingBoxes() % 2 == 1))) {
+			draw_bbox(node->bboxCenter.c,node->bboxSize.c);
+		}
+		//propagate bbox up one level
+		extent6f_copy(node->_extent,peek_group_extent());
+		pop_group_extent(); // up where parents are
+		union_group_extent(node->_extent); //
+	}
+
 
 	#ifdef CHILDVERBOSE
 	printf("RENDER ANCHOR END %d\n",node);
