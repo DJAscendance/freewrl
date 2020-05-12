8da778cbe4033d3ecb6f3676fcfae6dd9401f1d8
dug9 <highaspirations@hotmail.com>
2020-05-12 11:45:44 -0600

    Audio > src/libsound C wrapper on LabSound started
    - LabSound is (currently) outside freewrl project source control

---
+++ freex3d/src/libsound/libsound.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#if !defined(_WIN32)
+#define EXPORT_DLL
+#elif !defined(EXPORT_DLL)
+#if defined(_LIB)
+#define EXPORT_DLL
+#elif defined(_USRDLL)
+#define EXPORT_DLL __declspec(dllexport)
+#else
+#define EXPORT_DLL __declspec(dllimport)
+#endif
+#endif /* _WIN32 && EXPORT_DLL */
+
+EXPORT_DLL extern void* createContext();
+
