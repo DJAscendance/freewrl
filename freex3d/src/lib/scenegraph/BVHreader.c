d67d4c63c95be06e84360fe7ae2393b38141e9cc
dug9 <highaspirations@hotmail.com>
2020-05-19 07:23:32 -0600

    HAnim Motion BVHreader - needs joint name mappnig

--- freex3d/src/lib/scenegraph/BVHreader.c
+++ freex3d/src/lib/scenegraph/BVHreader.c
@@ -16,17 +16,14 @@ What X3D needs is HAnim2MotionData.
 #define ASSERT
 
 #include <config.h>
-#include <system.h>
-#include "Vector.h"
+#include <string.h>
+#include <malloc.h>
+#include <stdlib.h>
 #include "LinearAlgebra.h"
 
-// .H  >> 
-
-void read_bvh(char *file_path, float global_scale,
-	Stack *bvh_nodes, float *bvh_frame_time, int *bvh_frame_count);
-
-// << .H
-
+#define TRUE 1
+#define FALSE 0
+#define NULL ((void *)0)
 
 #ifdef _MSC_VER
 #define strcasecmp stricmp
@@ -75,56 +72,6 @@ static int chan_lookup(char *cname){
 
 
 
-#define TRUE 1
-#define FALSE 0
-struct eul_order {
-	int order[3];
-	char *str;
-} eul_orders [] = {
-	{-1,-1,-1,"XYZ"},  // XXX Dummy one, no rotation anyway!
-    {0, 1, 2, "XYZ"},
-    {0, 2, 1, "XZY"},
-    {1, 0, 2, "YXZ"},
-    {1, 2, 0, "YZX"},
-    {2, 0, 1, "ZXY"},
-    {2, 1, 0, "ZYX"},
-};
-char * eul_order_lookup (int *order) {
-	for(int i=0;i<7;i++){
-		int match = TRUE;
-		for(int j=0;j<3;j++)
-			match = match && order[j] == eul_orders[i].order[j];
-		if(match){
-			return eul_orders[i].str;
-		}
-	}
-	return eul_orders[0].str;
-}
-
-struct anim_record{
-	float lxyz[3];
-	float rxyz[3];
-};
-
-
-struct BVH_Node {
-	char *name; // bvh joint name
-	struct BVH_Node *parent;  // BVH_Node type or None for no parent
-	Stack * children;  // a list of children of this type.
-    float rest_head_world[3];  // worldspace rest location for the head of this node
-    float rest_head_local[3];   // localspace rest location for the head of this node
-    float rest_tail_world[3];  // worldspace rest location for the tail of this node
-    float rest_tail_local[3];  // worldspace rest location for the tail of this node
-	float rest_tail_local_store[3];
-	int channels[7];  // list of 6 ints, -1 for an unused channel, otherwise an index for the BVH motion data lines, loc triple then rot triple
-	int rot_order[3];  // a triple of indices as to the order rotation is applied. [0,1,2] is x/y/z - [None, None, None] if no rotation.
-    char * rot_order_str; // same as above but a string 'XYZ' format.
-    Stack *anim_data; //[6];  // a list one tuple's one for each frame. (locx, locy, locz, rotx, roty, rotz), euler rotation ALWAYS stored xyz order, even when native used.
-    BOOL has_loc;  // Convenience function, bool, same as (channels[0]!=-1 or channels[1]!=-1 or channels[2]!=-1)
-    BOOL has_rot;  // Convenience function, bool, same as (channels[3]!=-1 or channels[4]!=-1 or channels[5]!=-1)
-    int index;  // index from the file, not strictly needed but nice to maintain order
-    float *temp;  // use this for whatever you want
-};
 
 char * getline(char *line, int maxlen, char **position){
 	char *cur = *position;
@@ -136,47 +83,6 @@ char * getline(char *line, int maxlen, char **position){
 	*position = &cur[len+1];
 	return *position;
 }
-struct BVH_Node * init_BVH_Node( char *name, float * rest_head_world, float * rest_head_local, 
-	struct BVH_Node *parent, int *channels, int *rot_order, int index){
-	struct BVH_Node *self = (struct BVH_Node*)malloc(sizeof(struct BVH_Node));
-	memset(self,0,sizeof(struct BVH_Node));
-
-	self->name = name;
-	veccopy3f(self->rest_head_world,rest_head_world);
-	veccopy3f(self->rest_head_local,rest_head_local);
-	vecset3f(self->rest_tail_world,-1.0f,-1.0f,-1.0f);
-	vecset3f(self->rest_tail_local,-1.0f,-1.0f,-1.0f);
-	self->parent = parent;
-	memcpy(self->channels,channels,3*sizeof(int));
-	memcpy(self->rot_order,rot_order,3*sizeof(int));
-	self->rot_order_str = eul_order_lookup(self->rot_order);
-	self->index = index;
-
-	// convenience functions
-	self->has_loc = channels[0] != -1 || channels[1] != -1 || channels[2] != -1;
-	self->has_rot = channels[3] != -1 || channels[4] != -1 || channels[5] != -1;
-
-	self->children = newStack(struct BVH_Node*);
-
-	// list of 6 length tuples: (lx,ly,lz, rx,ry,rz)
-	// even if the channels aren't used they will just be zero
-	//
-	self->anim_data = newStack(struct anim_record); // [(0, 0, 0, 0, 0, 0)]
-	return self;
-}
-
-    //def __repr__(self):
-    //    return ("BVH name: '%s', rest_loc:(%.3f,%.3f,%.3f), rest_tail:(%.3f,%.3f,%.3f)" %
-    //            (self.name,
-    //             self.rest_head_world.x, self.rest_head_world.y, self.rest_head_world.z,
-    //             self.rest_head_world.x, self.rest_head_world.y, self.rest_head_world.z))
-
-
-//void sorted_nodes(struct BVH_Nodes *bvh_nodes){
-//    bvh_nodes_list = list(bvh_nodes.values())
-//    bvh_nodes_list.sort(key=lambda bvh_node: bvh_node.index)
-//}
-//
 
 struct joint_frame_motion {
 	char *jname;
@@ -307,10 +213,7 @@ void read_bvh_blob(char *blob, struct joint_frame_motion **chan, int *njoint, in
 	printf("njoint %d \n",*njoint);
 	struct joint_frame_motion *cchan = malloc(*njoint *sizeof(struct joint_frame_motion));
 	*chan = cchan;
-	for(int i=0;i<*njoint;i++){
-		printf("%d\n",i);
-		memcpy(&cchan[i],&cjoint[i],sizeof(struct joint_frame_motion));
-	}
+	memcpy(cchan,cjoint,mjoint * sizeof(struct joint_frame_motion));
  
 	float * fvalues = malloc( (*channel_count) * (*bvh_frame_count) * sizeof(float));
 	*values = fvalues;
@@ -325,6 +228,34 @@ void read_bvh_blob(char *blob, struct joint_frame_motion **chan, int *njoint, in
 			k++;
 		}
 	}
+
+	//convert degrees to radians
+	for(int iframe=0;iframe< *bvh_frame_count;iframe++){
+		float *fv = &fvalues[iframe * (*channel_count)];
+		int kchan = 0;
+		for(int j=0;j<mjoint;j++){
+			//printf("%s %d \n",vector_get(char*,jnames,j),chan[j].nchan);
+			for(int k=0;k<cchan[j].nchan;k++){
+				if(cchan[j].ichan[k] < 4)
+					fv[kchan] *= RADIANS_PER_DEGREE; //PI / 180.0; //
+				//printf("%d %5.2f ",chan[j].ichan[k],chan[j].ichan[k] < 4 ? fv[kchan]*180.0/PI : fv[kchan]);
+				kchan++;
+			}
+			//printf("\n");
+		}
+	}
+
+}
+
+void map_mocap_to_hanim_loa( struct joint_frame_motion *chan, int mjoint, int loa){
+
+	//map mocap joint names to HAnim2 loa joint names - see section 4.4.4 Joint mapping example
+	printf("=========\n");
+	for(int i=0;i<mjoint;i++){
+		//printf("%d %s\n",i,cjoint[i].mocap_name);
+		printf("%d %s\n",i,chan[i].mocap_name);
+	}
+	printf("=========\n");
 
 }
 
