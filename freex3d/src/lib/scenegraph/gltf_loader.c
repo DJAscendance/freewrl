1d2a28f148592ff42a3e94e8b9ad79f1827df738
dug9 <highaspirations@hotmail.com>
2022-04-11 12:15:08 -0600

    gltf_loader - set occlusionStrength

--- freex3d/src/lib/scenegraph/gltf_loader.c
+++ freex3d/src/lib/scenegraph/gltf_loader.c
@@ -677,6 +677,7 @@ int parse_gltf_node(struct X3D_Node *ectx, struct X3D_Node **spot, cgltf_data * 
 										it->url.n = 1;
 									}
 									mat->occlusionTexture = image;
+									mat->occlusionStrength = prim->material->occlusion_texture.scale;
 									if(do_mapping) mat->occlusionTextureMapping = newASCIIString("one");
 
 								}
@@ -816,6 +817,7 @@ int parse_gltf_node(struct X3D_Node *ectx, struct X3D_Node **spot, cgltf_data * 
 										it->url.n = 1;
 									}
 									mat->occlusionTexture = image;
+									mat->occlusionStrength = prim->material->occlusion_texture.scale;
 									if(do_mapping) mat->occlusionTextureMapping = newASCIIString("one");
 
 								}
@@ -997,12 +999,12 @@ int parse_gltf_node(struct X3D_Node *ectx, struct X3D_Node **spot, cgltf_data * 
 						printf("triangle fan\n");
 						break;
 				}
-				printf("adding shape to mesh group\n");
+				//printf("adding shape to mesh group\n");
 				//vector_pushBack(void *, &gr->children, sn);
 				AddRemoveChildren(X3D_NODE(gr), offsetPointer_deref(void*, gr, offsetof(struct X3D_Group, children)), &X3D_NODE(sn), 1, 1, __FILE__, __LINE__);
 			}
 		}
-		printf("adding mesh\n");
+		//printf("adding mesh\n");
 		vector_pushBack(void *, pp, gr);
 	}
 	if(node->skin){
