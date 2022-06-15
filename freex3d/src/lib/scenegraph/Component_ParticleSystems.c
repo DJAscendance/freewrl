05b0ef89a57470e7162d415d86a9d9ce41df7a4d
dug9 <highaspirations@hotmail.com>
2022-06-14 19:20:58 -0600

    ParticalSystems > touchup to protocol to match recent Shape and textureTransform_start

--- freex3d/src/lib/scenegraph/Component_ParticleSystems.c
+++ freex3d/src/lib/scenegraph/Component_ParticleSystems.c
@@ -1400,6 +1400,8 @@ void child_ParticleSystem(struct X3D_ParticleSystem *node){
 		//send materials, textures, matrices to shader
 		clear_textureUnit_used(); //appearance.texture material.textureXXX, PTMs.texture all need TEXTURE0+ XXX, where xxx starts from 0
 		clear_material_samplers(); //PTM and material.textureXXX share frag shader sampler2D textureUnit[16] array
+		clear_materialparameters_per_draw_counts(); //especially diffuse texture counts which both appearance and material share
+
 		textureTransform_start();
 		// maybe too much? resend_textureprojector_matrix();  
 		setupShaderB();
