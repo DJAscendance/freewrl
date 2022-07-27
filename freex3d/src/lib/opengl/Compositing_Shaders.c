ce0f02e52d3c42307cff2daa585c1ddfb5f11dbe
dug9 <highaspirations@hotmail.com>
2022-07-27 15:30:37 -0600

    VolumeRendering > ShadedVolumeStyle bug fix

--- freex3d/src/lib/opengl/Compositing_Shaders.c
+++ freex3d/src/lib/opengl/Compositing_Shaders.c
@@ -4071,6 +4071,93 @@ int getSpecificShaderSourceCastlePlugs (const GLchar **vertexSource, const GLcha
 }
 
 // START MIT, VOLUME RENDERING >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+static const GLchar* plug_vertex_lighting_ADSLightModel_volume = "\n\
+/* use ADSLightModel here the ADS colour is returned from the function.  */ \n\
+void PLUG_add_light_contribution2 (inout vec3 vertexcolor, inout vec3 specularcolor, in vec4 myPosition, in vec3 myNormal, \n\
+		in float mat_shininess, in float mat_ambient, in vec3 mat_diffuse, in vec3 mat_specular){ \n\
+	//working in eye space: eye is at 0,0,0 looking generally in direction 0,0,-1 \n\
+	//myPosition, myNormal - of surface vertex, in eyespace \n\
+	//vertexcolor - diffuse+ambient -will be replaced or modulated by texture color \n\
+	//specularcolor - specular+emissive or non-diffuse (emissive added outside this function) \n\
+	//algo: uses Blinn-Phong specular reflection: half-vector pow(N*H,shininess) \n\
+	// https://www.web3d.org/documents/specifications/19775-1/V3.3/Part01/components/lighting.html#Lightingequations \n\
+	// fog and emissive are done elsewhere, this function does: \n\
+	// SUM(on[i] x attenuation[i] x spot[i] x ILrgb[i] x (ambient[i] + diffuse[i] + specular[i])) \n\
+	int i; \n\
+	vec3 N = normalize (myNormal); \n\
+		\n\
+	vec3 E = -normalize(myPosition.xyz); \n \
+		\n\
+	// apply the lights to this material \n\
+	// weird but ANGLE needs constant loop \n\
+	vec3 sum_vertex = vec3(0.,0.,0.); \n\
+	vec3 sum_specular = vec3(0.,0.,0.); \n\
+	for (i=0; i<lightcount; i++) {\n\
+		vec3 diffuse = vec3(0., 0., 0.); \n\
+		vec3 ambient = vec3(0., 0., 0.); \n\
+		vec3 specular = vec3(0., 0., 0.); \n\
+		float on = 1.0; //we only send active/on lights to shader, so this is for radius \n\
+		float spot = 1.0; \n\
+		float attenuation = 1.0; //directional default \n\
+		fw_LightSourceParameters light = fw_LightSource[i]; \n\
+		int myLightType = lightType[i]; \n\
+		// VP vector of light direction and distance \n\
+		vec3 VP = light.location.xyz - myPosition.xyz; \n\
+		vec3 L = -light.direction; //directional light \n\
+		if(myLightType < 2){ \n\
+			//point and spot \n\
+			L = normalize(VP); \n\
+			float D = length(VP);  // distance to vertex \n\
+			// are we within range? \n\
+			if (D > light.lightRadius) on = 0.0; \n\
+			attenuation = 1.0/max(1.0,(light.Attenuations.x + (light.Attenuations.y * D) + (light.Attenuations.z *D*D))); \n\
+		} \n\
+		float NdotL = max(dot(N, L), 0.0); //Lambertian diffuse term \n\
+		//specular reflection models, phong or blinn-phong \n\
+		//#define PHONG 1 \n\
+		#ifdef PHONG \n\
+			//Phong \n\
+			vec3 R = normalize(-reflect(L,N)); \n\
+			float RdotE = max(dot(R,E),0.0); \n\
+			float specbase = RdotE; \n\
+			// assume shader gets shininess in 0 to 1 range, and scales it to 0 to 128 range here \n\
+			float specpow = mat_shininess*128.0; \n\
+		#else //PHONG \n\
+			//Blinn-Phong \n\
+			vec3 H = normalize(L + E); //halfvector x3d specs this is L+v/|L+v|\n\
+			float NdotH = max(dot(N,H),0.0); \n\
+			float specbase = NdotH; \n\
+			float specpow = mat_shininess*128.0; \n\
+		#endif //PHONG \n\
+		float powerFactor = 0.0; // for light dropoff \n\
+		if (specbase > 0.0) { \n\
+			powerFactor = pow(specbase,specpow); \n\
+			// tone down the power factor if mat_shininess borders 0 \n\
+		} \n\
+			\n\
+		ambient += light.ambient * mat_diffuse * mat_ambient; \n\
+		specular += light.intensity * mat_specular *powerFactor; \n\
+		diffuse += light.intensity * mat_diffuse * NdotL; \n\
+		if (myLightType==1) { \n\
+			// SpotLight  \n\
+			spot = 0.0; \n\
+			float spotDot = dot (-L,light.direction); \n\
+			// check against spotCosCutoff \n\
+			if (spotDot > light.spotCutoff) { \n\
+				if(spotDot > light.spotBeamWidth) { \n\
+					spot = 1.0; \n\
+				} else { \n\
+					spot = (spotDot - light.spotCutoff)/(light.spotBeamWidth - light.spotCutoff); \n\
+				} \n\
+			} \n\
+		} \n\
+		sum_vertex   += on * attenuation * spot * light.color * (ambient + diffuse); \n\
+		sum_specular += on * attenuation * spot * light.color * (specular); \n\
+	} \n\
+	vertexcolor = clamp(sum_vertex + vertexcolor, 0.0, 1.0); \n\
+	specularcolor = clamp(sum_specular + specularcolor, 0.0, 1.0); \n\
+} \n\
+";
 
 /* Generic GLSL vertex shader, used on OpenGL ES. */
 static const GLchar *volumeVertexGLES2 = " \n\
@@ -4770,7 +4857,7 @@ void voxel_apply_SHADED (inout vec4 voxel, inout vec3 gradient) { \n\
 	// void add_light_contribution2(inout vec3 vertexcolor, inout vec3 specularcolor, in vec4 myPosition, in vec3 myNormal, \n\
 	//   in float mat_shininess, in float mat_ambient, in vec3 mat_diffuse, in vec3 mat_specular); \n\
 	vec4 vertex_eye4 = vec4(vertex_eye,1.0); \n\
-	/* PLUG: add_light_contribution2 (color, castle_ColorES, vertex_eye4, ng, fw_FrontMaterial.shininess, fw_FrontMaterial.ambient, fw_FrontMaterial.diffuse, fw_FrontMaterial..specular) */ \n\
+	/* PLUG: add_light_contribution2 (color, castle_ColorES, vertex_eye4, ng, fw_FrontMaterial.shininess, fw_FrontMaterial.ambient, fw_FrontMaterial.diffuse, fw_FrontMaterial.specular) */ \n\
 	// voxel.rgb = color.rgb; \n\
 	color = mix(color,castle_ColorES,dot(ng,normal_eye)); \n\
 	voxel.rgb = color.rgb; \n\
@@ -5099,7 +5186,7 @@ int getSpecificShaderSourceVolume (const GLchar **vertexSource, const GLchar **f
 			// plug first, then the lower level plug should find its tartget in the CompleteCode
 			AddDefine(SHADERPART_FRAGMENT,"LIT",CompleteCode); 
 			AddDefine(SHADERPART_FRAGMENT,"LITE",CompleteCode); 
-			Plug(SHADERPART_FRAGMENT,plug_vertex_lighting_ADSLightModel,CompleteCode,&unique_int);
+			Plug(SHADERPART_FRAGMENT,plug_vertex_lighting_ADSLightModel_volume,CompleteCode,&unique_int);
 			break;
 		case SHADERFLAGS_VOLUME_STYLE_SILHOUETTE:
 			AddDefine(SHADERPART_FRAGMENT,"SILHOUETTE",CompleteCode); 
