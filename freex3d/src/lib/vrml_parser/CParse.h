7615eadcd3e5620bd66af6e253ba262095f85b65
dug9 <highaspirations@hotmail.com>
2024-01-29 12:12:58 -0700

    CParse allow decimal in DEF name

--- freex3d/src/lib/vrml_parser/CParse.h
+++ freex3d/src/lib/vrml_parser/CParse.h
@@ -47,9 +47,10 @@ FirstChar = RestChar minus:
 0x2b,0x2d +-
 Sept 2015 - we now allow 0x3a colon : in First and Rest - && c!=0x3a
 Feb  2016 - however, allowing : broke COMPONENT Core:2 splitting -Core:2 comes out as one chunk now- changed that code
+Feb  2024 - allow . 0x2E && c!=0x2E
 */
 #define IS_ID_REST(c) \
- (c>0x20 && c!=0x22 && c!=0x23 && c!=0x27 && c!=0x2C && c!=0x2E  && c!=0x5B && \
+ (c>0x20 && c!=0x22 && c!=0x23 && c!=0x27 && c!=0x2C   && c!=0x5B && \
   c!=0x5C && c!=0x5D && c!=0x7B && c!=0x7D && c!=0x7F )
 #define IS_ID_FIRST(c) \
  (IS_ID_REST(c) && (c<0x30 || c>0x39) && c!=0x2B && c!=0x2D )
